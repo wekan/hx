b1c210a86279c939912dcbcf5304a61cb0cb3283
Lauri Ojansivu <x@xet7.org>
2022-08-29 08:05:52 +0300

    Added actions and activities to mongodb-csv-sqlite

--- prototypes/database/MongoDB/mongoexport-csv-sqlite/mongoexport.sh
+++ prototypes/database/MongoDB/mongoexport-csv-sqlite/mongoexport.sh
@@ -2,9 +2,13 @@ rm wekan.sqlite *.csv
 
 mongoexport --db=wekan --collection=users --type=csv --fieldFile=users-fields.txt --out=users.csv
 mongoexport --db=wekan --collection=accountSettings --type=csv --fieldFile=accountSettings-fields.txt --out=accountSettings.csv
+mongoexport --db=wekan --collection=actions --type=csv --fieldFile=actions-fields.txt --out=actions.csv
+mongoexport --db=wekan --collection=activities --type=csv --fieldFile=activities-fields.txt --out=activities.csv
 
 echo ".mode csv
 .import accountSettings.csv accountSettings
+.import actions.csv actions
+.import activities.csv activities
 .import users.csv users
 .quit" | sqlite3 wekan.sqlite
 

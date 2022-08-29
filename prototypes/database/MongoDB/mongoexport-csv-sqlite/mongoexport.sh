bb8b00718830ca3ba14133060f75cad1d0f6f4c9
Lauri Ojansivu <x@xet7.org>
2022-08-29 07:24:06 +0300

    Added accountSettings to mongoexport-csv-sqlite

--- prototypes/database/MongoDB/mongoexport-csv-sqlite/mongoexport.sh
+++ prototypes/database/MongoDB/mongoexport-csv-sqlite/mongoexport.sh
@@ -1,6 +1,10 @@
+rm wekan.sqlite *.csv
+
 mongoexport --db=wekan --collection=users --type=csv --fieldFile=users-fields.txt --out=users.csv
+mongoexport --db=wekan --collection=accountSettings --type=csv --fieldFile=accountSettings-fields.txt --out=accountSettings.csv
 
 echo ".mode csv
+.import accountSettings.csv accountSettings
 .import users.csv users
 .quit" | sqlite3 wekan.sqlite
 

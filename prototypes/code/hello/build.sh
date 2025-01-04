6175ca5752d4bb7b72337449ff68572efc0d2ed7
Lauri Ojansivu <x@xet7.org>
2025-01-04 20:15:51 +0200

    Updates

--- prototypes/code/hello/build.sh
+++ prototypes/code/hello/build.sh
@@ -141,12 +141,12 @@ do
                 #sudo chown -R $(id -u):$(id -g) $HOME/.npm $HOME/.meteor
 
             elif [[ "$OSTYPE" == "linux-musl" ]]; then
-                yes | apk add cmake mlocate haxe git zip unzip p7zip nano alpine-sdk mono lua5.4-dev lua5.4-rex-pcre2 lua-luv-dev luarocks5.4 php84 curl wget firefox-esr firefox chromium netsurf konqueror godot openjdk17-jdk
+                yes | apk add cmake mlocate haxe git zip unzip p7zip nano alpine-sdk mono lua5.4-dev lua5.4-rex-pcre2 lua-luv-dev luarocks5.4 php84 php84-mbstring curl wget firefox-esr firefox chromium netsurf konqueror godot openjdk17-jdk
                 luarocks-5.4 install lrexlib-pcre PCRE_DIR=/usr/local
                 luarocks-5.4 install environ
                 luarocks-5.4 install luasocket
                 luarocks-5.4 install luv
-                luarocks-5.4 install luautf8
+                luarocks-5.4 install lua-utf8
             elif [[ "$OSTYPE" == "darwin"* ]]; then
                 echo "macOS";
                 # Needs XCode installed.
@@ -450,7 +450,11 @@ do
             echo "Run Node.js"
             node .build/node/wekan.js
             echo "Run PHP"
-            php .build/php/index.php
+            if [[ "$OSTYPE" == "linux-musl" ]]; then
+              php84 .build/php/index.php
+            else
+              php .build/php/index.php
+            fi
             echo "Run Python3"
             python3 .build/python/wekan.py
             echo Done.

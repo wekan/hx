d63d7a4c8ca9eb190bb92927adbf03eaf5f3cb7f
Lauri Ojansivu <x@xet7.org>
2025-11-24 23:30:22 +0200

    Updates

--- prototypes/ui/html5/src/Main.hx
+++ prototypes/ui/html5/src/Main.hx
@@ -3,12 +3,13 @@ package ;
 import haxe.ui.HaxeUIApp;
 import haxe.ui.core.Component;
 import haxe.ui.macros.ComponentMacros;
+import haxe.ui.ComponentBuilder;
 
 class Main {
     public static function main() {
         var app = new HaxeUIApp();
         app.ready(function() {
-            var mainView:Component = ComponentMacros.buildComponent("assets/main-view.xml");
+            var mainView:Component = ComponentBuilder.fromFile("assets/main-view.xml");
             app.addComponent(mainView);
 
             app.start();
--- prototypes/ui/tinkweb/Server.hx
+++ prototypes/ui/tinkweb/Server.hx
@@ -1,6 +1,19 @@
-import tink.http.containers.*;
+#if js
 import tink.http.Response;
 import tink.web.routing.*;
+import tink.core.Outcome;
+import tink.io.Source;
+import tink.http.containers.NodeContainer;
+import tink.http.Header;
+import tink.http.Header.HeaderField;
+#elseif php
+import tink.http.Response;
+import tink.web.routing.*;
+import tink.core.Outcome;
+import tink.io.Source;
+import tink.http.containers.PhpContainer;
+import tink.http.Header.HeaderField;
+#end
 
 class Server {
     static function main() {
@@ -11,9 +24,27 @@ class Server {
 #end
         var router = new Router<Root>(new Root());
         container.run(function(req) {
-            return router.route(Context.ofRequest(req));
-//                .recover(OutgoingResponse.reportError);
+            trace("Received request: " + req.header.url);
+            return router.route(Context.ofRequest(req))
+                .map(outcome -> {
+                    var response = switch outcome {
+                        case Success(r):
+                            trace("Sending response");
+                            r;
+                        case Failure(e):
+                            trace("Error: " + Std.string(e));
+                            new OutgoingResponse(
+                                new ResponseHeader(500, "Internal Server Error", [
+                                    HeaderField.contentType("text/plain")
+                                ]),
+                                tink.Chunk.ofString("Internal Server Error")
+                            );
+                    };
+                    return response;
+                });
         });
+
+        // Removed the infinite loop to prevent blocking the PHP server
     }
 }
 
@@ -22,6 +53,12 @@ class Root {
     
     @:get('/')
     @:get('/$name')
-    public function hello(name = 'World')
-        return 'Hello, $name!';
+    public function hello(name = 'World'):OutgoingResponse {
+        return new OutgoingResponse(
+            new ResponseHeader(200, "OK", [
+                HeaderField.contentType("text/html")
+            ]),
+            tink.Chunk.ofString("<html><body><h1>Hello, $name!</h1></body></html>")
+        );
+    }
 }
